15575c46c536c6c1bd4ad80f7e3c531b40147d34
juliawiktoria <maybe.nott123@gmail.com>
2021-02-25 13:12:23 +0000

    run fixes

--- train.py
+++ train.py
@@ -276,7 +276,7 @@ def init_processes(rank, size, fn, args):
     """ Initialize the distributed environment. """
     os.environ['MASTER_ADDR'] = args.master_address
     os.environ['MASTER_PORT'] = '6020'
-    torch.cuda.set_device(args.local_rank)
+    torch.set_device(1)
     dist.init_process_group(backend='nccl', init_method='env://', rank=rank, world_size=size)
     fn(args)
     cleanup()
